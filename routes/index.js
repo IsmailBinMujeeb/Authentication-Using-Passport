6a01f7c7f8efa0d2c32340f4be82e2bf15abdd2d
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2024-12-27 20:12:27 +0530

    add dynamic data in index rout

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
-const { isAuthenticated } = require("../utils/isAuthenticate")
+const { isAuthenticated } = require("../utils/isAuthenticate");
+const userModel = require("../models/user-model");
 
-router.get('/', isAuthenticated, (req, res)=>{
-  res.render('index');
+router.get('/', isAuthenticated, async (req, res)=>{
+
+  try {
+
+    const user = await userModel.findOne({_id: req.session.passport.user});
+
+    if (!user) return res.status(404).send("User not found");
+
+    res.render('index', { user });
+  } catch (error) {
+    console.log(error)
+  }
+  
 });
 
 module.exports = router;
