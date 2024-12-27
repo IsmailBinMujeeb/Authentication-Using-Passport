3f7f4843e309a7ee66684d524a5ec94274b14f7f
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2024-12-27 19:41:39 +0530

    Add user model

---
+++ models/user-model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+
+    fullname: {
+
+        type: String,
+        require: true,
+    },
+
+    username: {
+
+        type: String,
+        require: true,
+        unique: true,
+    },
+
+    email: {
+
+        type: String,
+        require: true,
+        unique: true
+    },
+
+    password: {
+
+        type: String,
+        require: true,
+    }
+})
+
+module.exports = mongoose.model("users", userSchema);
